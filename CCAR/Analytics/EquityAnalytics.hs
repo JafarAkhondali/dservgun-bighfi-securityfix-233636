cd14b23e049ccccc13d42f5ea54e63b2c78e52fa
Dinkar Ganti <dinkar.ganti@gmail.com>
2016-02-28 09:01:52 -0500

    Added equity analytics

--- CCAR/Analytics/EquityAnalytics.hs
+++ CCAR/Analytics/EquityAnalytics.hs
@@ -26,6 +26,9 @@ import							CCAR.Data.MarketDataAPI as MarketDataAPI
 import							Control.Monad.State as State
 import 							Data.Time
 import							Data.Functor.Identity as Identity
+import            				Math.Combinatorics.Exact.Binomial 
+
+
 iModuleName  = "CCAR.Analytics.EquityAnalytics"
 
 {-- Beta is computed as a magnitude of the change, it is roughly defined as follows
@@ -71,3 +74,61 @@ computeChangeI input evaluotorFunction =
 		(_, changeArray) = computeChange input
 
 
+
+
+average ::(Fractional a, Real a1) => [a1] -> a 
+average xs = realToFrac (sum xs) / fromIntegral (List.length xs)
+
+{-- Probability mass function to compute some mass histogram for a probability
+	
+ --}
+probabilityMassFunction :: Integral a => a -> a -> Double -> Double 
+probabilityMassFunction k n p = (fromIntegral (n `choose` k)) * (p^k) * ((1 - p)^(n - k))
+
+
+standardDeviation :: [Double] -> Double
+standardDeviation values = (sqrt . sum $ List.map (\x -> (x - mu) * (x - mu)) values) /sqrt_nm1
+			where 
+				mu = average values
+				sqrt_nm1 = sqrt $ (genericLength values - 1)
+
+{-- Compute standard error. I see how precicion was built-into statistics --}
+standardError = \input -> standardDeviation input/ (sqrt $ genericLength input)
+
+
+{-- Compute the variance --}
+variance :: [Double] -> Double
+variance values = (sum $ List.map (\x -> (x - mu) * (x - mu)) values) /sqrt_nm1
+			where 
+				mu = average values
+				sqrt_nm1 = genericLength values
+
+{-- covariance x y :--}
+covariance :: [Double] -> [Double] -> Double
+covariance x y = average $ List.zipWith (\xi yi -> (xi - xavg) *(yi - yavg)) x y 
+				where 
+					xavg = average x 
+					yavg = average y
+
+{-- Pearson r correlation coefficient --}
+pearsonR :: [Double] -> [Double] -> Double
+pearsonR x y = r 
+	where 
+		xstddev = standardDeviation x 
+		ystddev = standardDeviation y 
+		r = covariance x y / (xstddev * ystddev)
+
+pearsonRSquared :: [Double] -> [Double] -> Double
+pearsonRSquared x y = pearsonR x y ^ 2
+
+{-- Linear regression or beta to find a best-fit line.--}
+type Gradient = Double
+type Intercept = Double
+linearRegression :: [Double] -> [Double] -> (Gradient, Intercept)
+linearRegression x y = (gradient, intercept) 
+	where
+		xavg = average x 
+		yavg = average y 
+		gradient = covariance x y / (variance x)
+		intercept = yavg - (gradient * xavg)
+		
