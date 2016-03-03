2692da78e1df68c98b6ac58312d3ec49aea06f03
Dinkar Ganti <dinkar.ganti@gmail.com>
2016-03-03 00:30:52 -0500

    Equity benchmark

---
+++ CCAR/Data/EquityBenchmark.hs
@@ -0,0 +1,36 @@
+module CCAR.Data.EquityBenchmark
+	(startup) where 
+
+import Data.Text as T
+import CCAR.Parser.CSVParser as CSVParser(parseCSV, ParseError, parseLine)
+import System.IO 
+import System.Environment(getEnv)
+import Data.Conduit ( ($$), (=$=), (=$), Conduit, await, yield)
+import Data.Conduit.Binary as B (sinkFile, lines, sourceFile) 
+import Data.Conduit.List as CL 
+import Data.ByteString.Char8 as BS(ByteString, pack, unpack) 
+import Database.Persist as DB
+import Database.Persist.TH 
+import CCAR.Parser.CSVParser as CSVParser(parseCSV, ParseError, parseLine)
+import CCAR.Main.DBUtils
+
+import Control.Monad.Trans.Maybe
+import Control.Monad.IO.Class
+
+readLines :: (Monad m, MonadIO m) => Conduit BS.ByteString m (Either ParseError [String])
+readLines = undefined
+saveLine :: (MonadIO m) => Conduit (Either ParseError [String]) m (ByteString)
+saveLine =  undefined
+deleteLine :: (MonadIO m) => Conduit (Either ParseError [String]) m (ByteString)
+deleteLine = undefined
+
+
+type Symbol = T.Text
+type Benchmark = T.Text
+--persistEquityBenchmark ::  Symbol -> Benchmark -> IO EquityBenchmark
+persistEquityBenchmark s b = dbOps $ do 
+	x <- runMaybeT $ do 
+			liftIO $ do  
+				Nothing <- getBy $ UniqueBenchmark s b 
+				return $ DB.insert $ EquityBenchmark s b 
+	return x
