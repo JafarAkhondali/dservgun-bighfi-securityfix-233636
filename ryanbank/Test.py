ca78279ab3606d389c7852f954f5190871333af3
Dinkar Ganti <dinkar.ganti@gmail.com>
2016-03-10 12:10:35 -0500

    support csv

--- ryanbank/Test.py
+++ ryanbank/Test.py
@@ -4,6 +4,7 @@ import sys # To process command line arguments.
 import logging
 import os
 import traceback
+import argparse
 from timeit import timeit
 
 def headers(headerSize): 
@@ -50,7 +51,13 @@ def createChunkedDataSet(aNode, aSize):
 	chunkSize = min(aSize, 1000)
 	aDataSet = aNode.create_dataset(dsName, (aSize, 1), data = arr, dtype='f', chunks=(chunkSize, 1))
 
-	
+def insertIntoCSVFile(fHandle, aChunkSize): 
+	logging.debug("Creating a csv file");
+	arr = np.random.rand(aChunkSize, 1);
+	for num in arr:
+		fHandle.write("%f\n" % num)	
+
+
 # Combine keys to tag a pnl vector
 def combineKeys(key1, key2, pnl1):
 	pass
@@ -76,27 +83,45 @@ def summaryPnl(pnlList):
 			currentList = mplus(prevList, currentList)
 	return currentList
 
-def insert(aFile, defaultMode, args) :
+
+
+
+# Create a directory with the file name.
+# Create a header file for each header
+# Populate with the chunksize
+def insertCSV(aDirectoryName, chunkSize, headerSize):
+	# Creates a directory under the current directory.
+	os.mkdir(os.path.join(".", aDirectoryName))	
+	hs = headers(headerSize) 
+	for (x, y) in hs:
+		fileName = os.path.join(".", aDirectoryName, x)
+		filehandle = open(fileName, "a")
+		insertIntoCSVFile(filehandle, chunkSize);
+		filehandle.close()
+
+# Create a row in the file with a chunk size for the pnl vector
+# and headers
+def insert(aFile, defaultMode, chunkSize, headerSize) :
 	f = createFile(aFile, defaultMode)
 	try: 
 		flushCounter = 50;
 		currentCounter = 0;
-		hs = headers(int(args[3]))
+		hs = headers(headerSize)
 		for (x, y) in hs :
 			if(currentCounter % flushCounter == 0) :
 				logging.debug("Flushing " + str(f));
 				f.flush()
 			currentCounter = currentCounter + 1
 			group = createGroup(f, x, y);
-			createChunkedDataSet(group, int(args[2]))
+			createChunkedDataSet(group, chunkSize)
 		
 	except:
 		logging.error(traceback.format_exc())
 		logging.error("Error creating dataset. Deleting file");
-		os.remove(os.path.join(".", args[1]))
+		os.remove(os.path.join(".", aFile))
 
-def read(aFileName, readMode, args):
-	hs = headers(int(args[3]))
+def read(aFileName, readMode, chunkSize, headerSize):
+	hs = headers(headerSize)
 	f = createFile(aFileName, readMode);
 	for (x, y) in hs: 
 		g = getGroup(f, x, y);
@@ -111,12 +136,30 @@ def read(aFileName, readMode, args):
 	f.flush()
 	closeFile(f)
 
+'''
+To use the hdf5 option, being boolean, the canonical way of calling the script is as follows
+---- To use hdf5 file: 
+python Test.py --file test.hdf5 --hdf5 --chunkSize 1024 --headers 10
+---- To switch hdf5 off
+python Test.py --file test.hdf5 --chunkSize 1024 --headers 10
+'''
+def parseArguments() :
+	parser = argparse.ArgumentParser(description = "A simple utility to compare file systems: hdf5 and a flat file.");
+	parser.add_argument("--file", help="Data file")
+	parser.add_argument("--hdf5", help="Save in hdf5 or as a csv file", action='store_true', default=False)
+	parser.add_argument("--chunkSize", type=int, help="pnl vector size")
+	parser.add_argument("--headers", type=int, help="risk factors")
+	return parser.parse_args()
 def main():
 	logging.basicConfig(level=logging.INFO)
 	defaultMode = "a"
-	args = sys.argv
-	insert(args[1], defaultMode, args)
-	read(args[1], "r", args)
+	namespace = parseArguments();
+	print (str(namespace))	
+	if (namespace.hdf5 == True): 
+		insert(namespace.file, defaultMode, namespace.chunkSize, namespace.headers)
+	else:
+		insertCSV(namespace.file, namespace.chunkSize, namespace.headers)
+	#read(namespace.file, "r", namespace.chunkSize, namespace.headers)
 if __name__ == '__main__':
 	logging.info("Time taken " + str(timeit(main, number = 1)))
 
