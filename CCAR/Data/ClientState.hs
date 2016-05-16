bec8c23dc2dd3a5b45fda5345ecb60aa87593230
Dinkar Ganti <dinkar.ganti@gmail.com>
2016-05-15 21:05:53 -0400

    Created a library, and test suite framework

---
+++ CCAR/Data/ClientState.hs
@@ -0,0 +1,72 @@
+module CCAR.Data.ClientState
+    (ClientState(..)
+    , ClientIdentifier
+    , ClientIdentifierMap
+    , ActivePortfolio
+    , createClientState
+    , makeActivePortfolio) where 
+
+import qualified Data.Map as Map
+import Control.Concurrent.STM.Lifted 
+import Data.Text as T
+import Data.Monoid 
+import Data.Time(UTCTime)
+import Network.WebSockets.Connection as WSConn (Connection)
+import Data.Aeson 
+import CCAR.Parser.CCARParsec
+import CCAR.Model.PortfolioT
+import CCAR.Analytics.OptionPricer
+import Data.Typeable
+import Data.Data
+import GHC.Generics
+
+
+type ClientIdentifier = T.Text
+newtype ActivePortfolio = ActivePortfolio {unP :: PortfolioT} 
+        deriving(Show, Read, Eq, Data, Generic, Typeable)
+
+makeActivePortfolio = ActivePortfolio 
+
+data ClientState = ClientState {
+			nickName :: ClientIdentifier
+			, connection :: WSConn.Connection
+			, readChan :: TChan T.Text
+			, writeChan :: TChan T.Text
+            , jobReadChan :: TChan Value 
+            , jobWriteChan :: TChan Value
+            , workingDirectory :: FilePath
+            , activeScenario :: [Stress]
+            , pricerReadQueue :: TBQueue OptionPricer
+            , lastMessageType :: T.Text
+            , lastUpdateTime :: UTCTime -- The last update time for a message.
+            , activePortfolio :: Maybe ActivePortfolio -- The currently selected portfolio.
+	}
+
+
+createClientState :: ClientIdentifier -> WSConn.Connection -> UTCTime -> STM ClientState
+createClientState nn aConn currentTime = do 
+        w <- newTChan
+        r <- dupTChan w 
+        jw <- newTChan 
+        jwr <- dupTChan jw
+        pricerReadQueue <- newTBQueue 5 
+        return ClientState{nickName = nn 
+                        , connection = aConn
+                        , readChan = r 
+                        , writeChan = w
+                        , jobWriteChan = jw 
+                        , jobReadChan = jwr
+                        , workingDirectory = ("." <> (T.unpack nn))
+                        , activeScenario = []
+                        , pricerReadQueue = pricerReadQueue
+                        , lastMessageType = ""
+                        , lastUpdateTime = currentTime
+                        , activePortfolio = Nothing
+        }
+
+
+instance Show ClientState where 
+    show cs = (show $ nickName cs) ++  " Connected"
+type ClientIdentifierMap = TVar (Map.Map ClientIdentifier ClientState)
+
+
---
+++ CCAR/Model/PortfolioT.hs
@@ -0,0 +1,120 @@
+module CCAR.Model.PortfolioT 
+	(PortfolioT(..)
+	, PortfolioQuery(..)
+	, CompanyID(..)
+	, NickName(..)
+	, PortfolioUUID(..)
+	, CRUD(..)) where 
+
+import Data.Aeson
+import Data.Aeson.Types
+import Control.Applicative as Appl ((<*>), (<$>), empty)
+import Data.Text as T 
+import Data.Typeable
+import Data.Data
+import GHC.Generics
+import CCAR.Main.DBUtils
+import Data.Monoid
+-- Data types
+
+
+data CRUD = Create | Read | P_Update | Delete 
+	deriving(Show, Read, Eq, Data, Generic, Typeable)
+
+newtype PortfolioUUID = PortfolioUUID {unP :: T.Text} 
+	deriving (Show, Read, Eq, Data, Generic, Typeable)
+
+newtype CompanyID = CompanyID {unC :: T.Text}
+	deriving (Show, Read, Eq, Data, Generic, Typeable) 
+
+instance ToJSON CRUD 
+instance FromJSON CRUD
+instance ToJSON CompanyID 
+instance FromJSON CompanyID
+instance ToJSON NickName 
+instance FromJSON NickName
+
+
+data PortfolioT = PortfolioT {
+	crudType :: CRUD
+	, portfolioId :: PortfolioUUID
+	, companyId :: CompanyID
+	, userId :: NickName
+	, summary :: T.Text 
+	, createdBy :: NickName
+	, updatedBy :: NickName
+} deriving(Show, Read, Eq, Data, Generic, Typeable)
+
+
+data PortfolioQuery = PortfolioQuery {
+	pqCommandType :: T.Text
+	, pqNickName :: NickName
+	, qCompanyId :: CompanyID 
+	, qUserId :: NickName
+	, resultSet :: [Either T.Text PortfolioT]
+} deriving (Show, Read, Eq, Data, Generic, Typeable)
+
+instance ToJSON PortfolioUUID 
+instance FromJSON PortfolioUUID
+instance ToJSON PortfolioQuery where 
+	toJSON pq@(PortfolioQuery cType nickName qCid userId r) = 
+		object [
+			"commandType" .= cType 
+			, "nickName" .= nickName 
+			, "companyId" .= qCid 
+			, "useerId"  .= userId 
+			, "resultSet" .= r
+		]
+instance FromJSON PortfolioQuery where 
+	parseJSON (Object a)  = PortfolioQuery <$> 
+						a .: "commandType" <*> 
+						a .: "nickName" <*> 
+						a .: "companyId" <*> 
+						a .: "userId" <*> 
+						a .: "resultSet"
+	parseJSON _ 		 = Appl.empty
+
+instance ToJSON PortfolioT where
+	toJSON p1@(PortfolioT c p c1 u s cr up) =
+		object [
+			"crudType" .= c 
+			, "portfolioId" .= (unP p) 
+			, "companyId" .= (unC c1) 
+			, "userId" .= (unN u) 
+			, "summary" .= s 
+			, "createdBy" .=  (unN cr)
+			, "updatedBy" .= (unN up)
+			, "commandType" .= ("ManagePortfolio" :: T.Text)
+		]
+
+instance FromJSON PortfolioT where
+	parseJSON (Object a) = do 
+			cr <- a .: "crudType" 
+			p <- a .: "portfolioId" >>= return . PortfolioUUID
+			c <- a .: "companyId" >>= return . CompanyID 
+			u <- a .: "userId" >>= return . NickName
+			summ <- a .: "summary"
+			cre <- a .: "createdBy" >>= return . NickName
+			upd <- a .: "updatedBy" >>= return . NickName 
+			return $ PortfolioT cr p c u summ cre upd			
+{-
+		PortfolioT <$>
+				a .: "crudType" <*>
+				a .: "portfolioId" <*>
+				a .: "companyId" <*> 
+				a .: "userId" <*>
+				a .: "summary" <*>
+				a .: "createdBy" <*>
+				a .: "updatedBy" -}
+	parseJSON _ 	= Appl.empty  
+
+instance Monoid PortfolioUUID where 
+	mappend (PortfolioUUID p1) (PortfolioUUID p2) = PortfolioUUID (p1 <> p2)
+	mempty = PortfolioUUID ""
+
+testParse :: Result PortfolioT
+testParse =  parse parseJSON testtoJson
+
+testtoJson = toJSON $ PortfolioT Create (PortfolioUUID "1") 
+					(CompanyID "test") 
+					(NickName "test") ("test") (NickName "test") (NickName "test")
