b74ce113ef366252ba6abd024cc70838a587bcd3
Dinkar Ganti <dinkar.ganti@gmail.com>
2016-05-31 09:38:48 -0400

    skeletal interface.

--- client-wxpython/ods_python_script.py
+++ client-wxpython/ods_python_script.py
@@ -7,6 +7,69 @@ import websockets
 ##### We will break them down into modules as the size of the macros grow.
 
 
+##### Todo:
+##### Package the server interaction as a library.
+
+
+#Send and receive commands. XXX: Ordering of functions matter.
+
+
+
+def sendLoginRequest(userName, password) : 
+    #create a login json request
+    pass
+
+def handleLoginResponse(incomingJson) :
+    #Handle incoming json response
+    pass
+
+def handleUserNotFound (incomingJson) :
+    # If the user is not found, let the 
+    # register on the website. This 
+    # might be a useful feature, security wise
+    # A sort of poor man's tfa. 
+    # Issues to consider: handling multiple 
+    # active connections. 
+    # The server needs to count per client type.
+    # Browser, Smartphone, desktop
+    pass
+
+
+def keepAliveTimer () :
+    # A keep alive timer thread that starts
+    # once a user is validated.
+    pass
+
+def getCommandType(incomingJson) : 
+    # Return the command type for incoming message.
+    pass
+
+def parseIncomingMessage(incomingJson) : 
+    # Get the command type
+    # process the appropriate command type.
+    pass 
+
+def handleUndefinedCommandType(incomingMessage) :
+    # Do something when command type is not defined.
+    pass
+
+def sendManageCompany(aJsonMessage) :
+    # Send a manage company json request
+    pass
+def sendSelectAllCompaniesRequest(aJsonMessage) :
+    # A request to return all the companies
+    pass 
+
+def handleSelectAllCompaniesResponse(): 
+    # Server response to return all the companies
+    # Company reference is to help whitelabel the client.
+    pass
+
+def sendQuerySupportedScripts(aJsonRequest) : 
+    pass 
+def handleQuerySupportedScripts(aJsonResponse):
+    pass 
+## https returns and invalid url. 
 def clientConnection () : 
     #return "https://beta.ccardemo.tech/chat"
     return "ws://localhost:3000/chat"
