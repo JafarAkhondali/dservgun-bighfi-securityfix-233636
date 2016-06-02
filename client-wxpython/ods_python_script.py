71297ff07cdc8c4689c88c6a878a3b6814e655ed
Dinkar Ganti <dinkar.ganti@gmail.com>
2016-06-02 06:59:07 -0400

    Some more changes to the script

--- client-wxpython/ods_python_script.py
+++ client-wxpython/ods_python_script.py
@@ -2,26 +2,125 @@ import sys
 import urllib
 import asyncio
 import websockets
-##### Note: 
+import traceback
+import json
+##### Note: Requires python 3.4 or above
 ##### It seems to be that all functions in a macro need to reside in a single file.
 ##### We will break them down into modules as the size of the macros grow.
-
+##### 
 
 ##### Todo:
 ##### Package the server interaction as a library.
 
 
-#Send and receive commands. XXX: Ordering of functions matter.
+def convertToBool(aString):
+    if aString.capitalize() == "True" : 
+        return True
+    else: 
+        return False
+
+def SECURITY_CELL(): 
+    return "B15"
+
+def SECURITY_CELL_LOG(): 
+    return "B16"
 
 
+
+def parseIncomingMessage(incomingJson) : 
+    commandType = getCommandType(incomingJson)
+    if commandType == 0 :
+        handleLoginResponse(incomingJson)
+    elif commandType == 1 :
+        handleCCARUpload(incomingJson)
+    elif commandType == 2: 
+        handleManageCompany(incomingJson)
+    elif commandType == 3 :
+        handleSelectAllCompanies(incomingJson)
+    elif commandType == 4 : 
+        handleQuerySupportedScripts(incomingJson)
+    elif commandType == 5 : 
+        handleQueryActiveWorkbenches(incomingJson)
+    else:
+        pass
+def commandDictionary () :
+    {
+        "Login"                 : 0
+    ,   "CCARUpload"            : 1
+    , "ManageCompany"           : 2
+    , "SelectAllCompanies"      : 3
+    , "QuerySupportedScripts"   : 4
+    , "QueryActiveWorkbenches"  : 5
+    , "ManageWorkbench"         : 6
+    , "ExecuteWorkbench"        : 7
+    , "SelectActiveProjects"    : 8
+    , "ManageProject"           : 9
+    , "ParsedCCARText"          : 10
+    , "ManageUser"              : 11
+    , "CreateUserTerms"         : 12
+    , "UpdateUserTerms"         : 13
+    , "DeleteUserTerms"         : 14
+    , "QueryUserTerms"          : 15
+    , "CreateUserPreferences"   : 16
+    , "UpdateUserPreferences"   : 17
+    , "QueryUserPreferences"    : 18
+    , "DeleteUserPreferences"   : 19
+    , "SendMessage"             : 20
+    , "UserJoined"              : 21
+    , "UserBanned"              : 22
+    , "UserLoggedIn"            : 23
+    , "UserLeft"                : 24
+    , "AssignCompany"           : 25
+    , "keepAlive"               : 26
+    , "PortfolioSymbolTypesQuery" : 27
+    , "PortfolioSymbolSidesQuery" : 28
+    , "QueryPortfolios"         : 29
+    , "ManagePortfolio"         : 30
+    , "ManagePortfolioSymbol"   : 31
+    , "QueryPortfolioSymbol"    : 32
+    , "ManageEntitlements"      : 33
+    , "QueryEntitlements"       : 34
+    , "QueryCompanyUsers"       : 35
+    , "MarketDataUpdate"        : 36
+    , "OptionAnalytics"         : 37
+    , "QueryMarketData"         : 38
+    , "HistoricalStressValueCommand" : 39
+    , "Undefined"               : 40
+
+    }
+
+
+
+# Indexes are zero based.
+def getWorksheet(anIndex) :
+    desktop = XSCRIPTCONTEXT.getDesktop()
+    model = desktop.getCurrentComponent()
+    sheet = model.Sheets.getByIndex(anIndex)
+    return sheet
+
+def getSecuritySettings() :
+    # Return the security settings for this document.
+    desktop = XSCRIPTCONTEXT.getDesktop()
+    model = desktop.getCurrentComponent()
+    sheet = model.Sheets.getByIndex(0)
+    tRange = sheet.getCellRangeByName(SECURITY_CELL())
+    tRangeO = sheet.getCellRangeByName(SECURITY_CELL_LOG())
+    tRangeO.String = "Using " + tRange.String
+    return tRange.String
 
 def sendLoginRequest(userName, password) : 
     #create a login json request
-    pass
+    login = {
+        'commandType' : 'Login',
+        'nickName' : userName, 
+        'loginStatus' : "Undefined"
+
+    }
+    return json.dumps(login)
 
 def handleLoginResponse(incomingJson) :
-    #Handle incoming json response
-    pass
+    data = json.loads(incomingJson);
+
 
 def handleUserNotFound (incomingJson) :
     # If the user is not found, let the 
@@ -41,13 +140,21 @@ def keepAliveTimer () :
     pass
 
 def getCommandType(incomingJson) : 
-    # Return the command type for incoming message.
-    pass
+    data = json.loads(incomingJson);
+    try :
+        if data.has_key("commandType"):
+            cType =  data["commandType"]
+            return commandDictionary()[cType]
+        elif data.has_key("Right"):
+            cType = (data["Right"])["commandType"]
+            return commandDictionary()[cType]
+        else:
+            return commandDictionary()["Undefined"]
+    except :
+        updateErrorWorkSheet("InvalidCommandType " + incomingJson)
+        cType = commandDictionary()["Undefined"]
 
-def parseIncomingMessage(incomingJson) : 
-    # Get the command type
-    # process the appropriate command type.
-    pass 
+
 
 def handleUndefinedCommandType(incomingMessage) :
     # Do something when command type is not defined.
@@ -55,6 +162,9 @@ def handleUndefinedCommandType(incomingMessage) :
 
 def sendManageCompany(aJsonMessage) :
     # Send a manage company json request
+    pass
+def handleManageCompany(aJsonResponse): 
+    #Handle manage company
     pass
 def sendSelectAllCompaniesRequest(aJsonMessage) :
     # A request to return all the companies
@@ -215,29 +325,63 @@ def handleHistoricalStressValue(jsonResponse):
     pass
 
 
+def LOGIN_CELL() :
+    return"B5" 
+def PASSWORD_CELL() :
+    return "B6"
+
+def ERROR_CELL(): 
+    return "A29"
+def updateErrorWorksheet(aMessage) :
+    sheet = getWorksheet(0);
+    tRange = sheet.getCellRangeByName(ERROR_CELL())
+    tRange.String = aMessage    
+
+def updateCellContent(aCell, aValue) : 
+    sheet = getWorksheet(0);
+    tRange = sheet.getCellRangeByName(aCell)
+    tRange.String = aValue
+
+def getCellContent(aCell): 
+    sheet = getWorksheet(0);
+    tRange = sheet.getCellRangeByName(aCell)
+    return tRange.String
 ## https returns and invalid url. 
 def clientConnection () : 
     #return "https://beta.ccardemo.tech/chat"
     return "wss://beta.ccardemo.tech/chat"
 
 @asyncio.coroutine
-def hello(userName, password):
-    websocket = yield from websockets.connect(clientConnection())
+def ccarLoop(userName, password, useSsl):
+    websocket = yield from websockets.connect(clientConnection(), ssl=useSsl)
     try:
         yield from websocket.send(userName)
         print(userName)
-
         greeting = yield from websocket.recv()
         print(greeting)
-
     finally:
+        updateErrorWorksheet("Get the exception handler here")
         yield from websocket.close()
 
-def login (userName, password) :
-    asyncio.get_event_loop().run_until_complete(hello(userName, password))
-    return (userName + "_" + "***************")
 
+def LOGGER_CELL():
+    return "A23"
 
+def login (userName, password, ssl) :
+    try:
+        updateCellContent(LOGGER_CELL(), "Username" + "tbd-")
+        if userName == None or userName == "" or password == None or password == "":
+            updateCellContent(LOGGER_CELL(), "User name and or password not found")
+            return;
+        updateCellContent(LOGGER_CELL(), "After user name");
+        asyncio.get_event_loop().run_until_complete(ccarLoop(userName, password, ssl))
+        return (userName + "_" + "***************")
+    except:
+        error = traceback.format_exc() 
+        updateErrorWorksheet(error)
+        return "Error while logging in" 
+    finally:
+        return "Finished processing login"
 
 def PythonVersion(*args):
     """Prints the Python version into the current document"""
@@ -257,9 +401,12 @@ def PythonVersion(*args):
 #do the same for the python executable path
     tRange = sheet.getCellRangeByName("C5")
     tRange.String = sys.executable
-
     tRange = sheet.getCellRangeByName("C10")
-    tRange.String = login("test", "test")
+    s = getSecuritySettings()
+    l = getCellContent(LOGIN_CELL())
+    p = getCellContent(PASSWORD_CELL())
+    tRange.String = login(l, p, convertToBool(s))
+
     return None
 
 
