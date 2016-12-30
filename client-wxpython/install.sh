52539c94d6e82286af6f59c56af0661968bb9e74
Dinkar Ganti <dinkar.ganti@gmail.com>
2016-12-30 11:18:05 -0500

    Added installation instructions

--- client-wxpython/install.sh
+++ client-wxpython/install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export COPY="cp -v -p" 
-export DIRECTORY="."
-export SOURCE="ods_python_script.py"
-export TARGET="/usr/lib/libreoffice/share/Scripts/python/"
+export DIRECTORY="./with_macro/."
+export TARGET="../client-hx"
+export SOURCE="ClientTemplate.ods"
+
+# Create a copy of the template file
+# Run this ${PYTHON3} include_macro.py ./${SOURCE}
+# The new ods file gets created under with_macro. 
+# Edit the file under .ods to associate macros with the application. 
+# Run install dot sh.
 ${COPY} ${DIRECTORY}/${SOURCE} ${TARGET}
 echo `date`
 
-echo "Copying the bundle for "
-${COPY} ${DIRECTORY}/ca_bundle.pem.bak ${TARGET}
-${COPY} ${DIRECTORY}/ca_bundle.pem ${TARGET}
