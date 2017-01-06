4915aeaac7bd331c3610c3e3152b4f17d13faef3
Dinkar Ganti <dinkar.ganti@gmail.com>
2017-01-05 22:09:41 -0500

    Fixed the connection issue

---
+++ scripts/RunSeleniumServer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Run this script for tests.
+export JAVA=java
+export SELENIUM_DIR=selenium-server
+export SELENIUM_VERSION=2.53.0
+export SELENIUM_PROG_NAME=selenium-server-standalone
+export PS=`ps-ef`
+echo "Run the selenium server ${SELENIUM_DIR}"
+
+${PS} | grep ${SELENIUM_PROG_NAME} | grep -v grep | awk {print $2}
+
+#${JAVA} -jar "./${SELENIUM_DIR}/selenium-server-standalone-${SELENIUM_VERSION}.jar"
